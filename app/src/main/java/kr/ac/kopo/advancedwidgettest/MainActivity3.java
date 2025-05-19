3ac5086b53b90b6ded00dacbf50132b80ca9cf75
wjdtnd <gkssk15963@gmail.com>
2025-05-19 14:02:11 +0900

    250519 멀티오토컴플리트텍스트뷰

--- app/src/main/java/kr/ac/kopo/advancedwidgettest/MainActivity3.java
+++ app/src/main/java/kr/ac/kopo/advancedwidgettest/MainActivity3.java
@@ -30,5 +30,13 @@ public class MainActivity3 extends AppCompatActivity {
 
         ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, items);
         auto.setAdapter(adapter);
+
+//          여러개의 자동 완성기능 사용(컴마기준)
+        MultiAutoCompleteTextView.CommaTokenizer commaToken = new MultiAutoCompleteTextView.CommaTokenizer();
+        multi.setTokenizer(commaToken);
+        multi.setAdapter(adapter);
+
+
+
     }
 }
